1f4442ba5ab01483e4513d377b44cb9141e88637
nkmorozov <n.morozw06@gmail.com>
2026-07-13 15:35:06 +0300

    connecting redis

--- calls/consumers.py
+++ calls/consumers.py
@@ -1,19 +1,46 @@
 import json
-from collections import defaultdict
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.conf import settings
+from redis.asyncio import Redis
 
 
-ROOM_USERS = defaultdict(set)
+ROOM_LIMIT = 2
+ROOM_TTL_SECONDS = 6 * 60 * 60
+
+redis_client = Redis.from_url(settings.REDIS_URL, decode_responses=True)
+
+JOIN_ROOM_SCRIPT = """
+local key = KEYS[1]
+local channel_name = ARGV[1]
+local ttl = tonumber(ARGV[2])
+local limit = tonumber(ARGV[3])
+
+redis.call('SREM', key, channel_name)
+
+local previous_count = redis.call('SCARD', key)
+
+if previous_count >= limit then
+    return {-1, previous_count}
+end
+
+redis.call('SADD', key, channel_name)
+redis.call('EXPIRE', key, ttl)
+
+return {previous_count, previous_count + 1}
+"""
 
 
 class CallConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_id = str(self.scope['url_route']['kwargs']['room_id'])
         self.room_group_name = f'call_{self.room_id}'
-        self.is_initiator = len(ROOM_USERS[self.room_id]) == 0
+        self.room_users_key = f'call:{self.room_id}:users'
+        self.joined_room = False
 
-        if len(ROOM_USERS[self.room_id]) >= 2:
+        previous_count, users_count = await self.add_user_to_room()
+
+        if previous_count == -1:
             await self.accept()
             await self.send(text_data=json.dumps({
                 'type': 'room_full',
@@ -22,7 +49,8 @@ class CallConsumer(AsyncWebsocketConsumer):
             await self.close()
             return
 
-        ROOM_USERS[self.room_id].add(self.channel_name)
+        self.is_initiator = previous_count == 0
+        self.joined_room = True
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
@@ -32,13 +60,16 @@ class CallConsumer(AsyncWebsocketConsumer):
             'is_initiator': self.is_initiator,
         }))
 
-        await self.send_room_status()
+        await self.send_room_status(users_count)
 
     async def disconnect(self, close_code):
-        if hasattr(self, 'room_id'):
-            ROOM_USERS[self.room_id].discard(self.channel_name)
-            await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-            await self.send_room_status()
+        if not getattr(self, 'joined_room', False):
+            return
+
+        users_count = await self.remove_user_from_room()
+
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.send_room_status(users_count)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -52,12 +83,41 @@ class CallConsumer(AsyncWebsocketConsumer):
             },
         )
 
-    async def send_room_status(self):
+    async def add_user_to_room(self):
+        previous_count, users_count = await redis_client.eval(
+            JOIN_ROOM_SCRIPT,
+            1,
+            self.room_users_key,
+            self.channel_name,
+            ROOM_TTL_SECONDS,
+            ROOM_LIMIT,
+        )
+
+        return int(previous_count), int(users_count)
+
+    async def remove_user_from_room(self):
+        await redis_client.srem(self.room_users_key, self.channel_name)
+        users_count = await redis_client.scard(self.room_users_key)
+
+        if users_count == 0:
+            await redis_client.delete(self.room_users_key)
+        else:
+            await redis_client.expire(self.room_users_key, ROOM_TTL_SECONDS)
+
+        return int(users_count)
+
+    async def get_room_users_count(self):
+        return int(await redis_client.scard(self.room_users_key))
+
+    async def send_room_status(self, users_count=None):
+        if users_count is None:
+            users_count = await self.get_room_users_count()
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'room_status',
-                'users_count': len(ROOM_USERS[self.room_id]),
+                'users_count': users_count,
             },
         )
 
--- config/settings.py
+++ config/settings.py
@@ -55,12 +55,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 ASGI_APPLICATION = 'config.asgi.application'
 
+REDIS_URL = config('REDIS_URL', default='redis://127.0.0.1:6379/0')
+
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [REDIS_URL],
+        },
     },
 }
-
 
 DATABASES = {
     'default': {
